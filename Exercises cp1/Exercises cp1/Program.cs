053c4730c7dcc68c387eab26c0a2b414e09b73f0
sjory3 <37364942+sjory3@users.noreply.github.com>
2021-01-16 23:20:02 +0100

    cp1 and 2
    made chapter 1 and some of 2

---
+++ Exercises cp1/Exercises cp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+
+namespace Exercises_cp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ex11();
+            Console.ReadLine();
+        }
+
+        //Write a console application that prints your first and last name on the console.
+        static void ex6()
+        {
+            Console.WriteLine("Michael Jakobsen");
+            Console.ReadLine();
+        }
+
+        /*Write a program that prints the following numbers on the console 1,
+        101, 1001, each on a new line.
+        */
+        static void ex7()
+        {
+            Console.WriteLine("1");
+            Console.WriteLine("101");
+            Console.WriteLine("1001");
+        }
+
+        //Write a program that prints on the console the current date and time.
+        static void ex8()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+
+        //Write a program that prints the square root of 12345.
+        static void ex9()
+        {
+            Console.WriteLine(Math.Sqrt(12345));
+        }
+
+        //Write a program that prints the first 100 members of the sequence 2, -3, 4, -5, 6, -7, 8.
+        static void ex10()
+        {
+            bool test = true;
+            for (int i = 2; i < 103; i++)
+            {
+                if (test)
+                {
+                    Console.WriteLine(i);
+                    test = false;
+                }
+                else
+                {
+                    Console.WriteLine("-"+i);
+                    test = true;
+                }
+            }
+        }
+
+        //Write a program that reads your age from the console and prints your age after 10 years.
+        static void ex11()
+        {
+            Console.WriteLine("please enter your age");
+            int age = int.Parse(Console.ReadLine());
+            age = age + 10;
+            Console.WriteLine("youre age after 10 years is: " + age);
+        }
+    }
+}
---
+++ Exercises cp2/Exercises cp2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_cp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ex3();
+            Console.ReadLine();
+        }
+
+        /*Declare several variables by selecting for each one of them the most
+        appropriate of the types sbyte, byte, short, ushort, int, uint, long
+        and ulong in order to assign them the following values: 52,130; -115;
+        4825932; 97; -10000; 20000; 224; 970,700,000; 112; -44; -1,000,000;
+        1990; 123456789123456789.*/
+        static void ex1()
+        {
+            sbyte a = -115;
+            sbyte a2 = 97;
+            sbyte a3 = 112;
+            sbyte a4 = -44;
+
+            byte b = 224;
+
+            short c = -10000;
+            short c2 = 20000;
+            short c3 = 1990;
+
+            ushort d = 52130;
+
+            int e = 4825932;
+            int e2 = -1000000;
+            int e3 = 970700000;
+
+            long f = 123456789123456789;
+        }
+
+        /*Which of the following values can be assigned to variables of type float,
+        double and decimal: 5, -5.01, 34.567839023; 12.345; 8923.1234857;
+        3456.091124875956542151256683467? */
+        static void ex2()
+        {
+            //float: -5.01; 12.345; 5;
+            //double 3456.091124875956542151256683467;;
+            //decimal 34.567839023; 8923.1234857;
+        }
+
+        //Write a program, which compares correctly two real numbers with accuracy at least 0.000001.
+        static void ex3()
+        {
+            float a = 10.000002f;
+            float b = 10.000002f;
+
+            if (a == b)
+            {
+                Console.WriteLine("its the same");
+            }
+            else
+            {
+                Console.WriteLine("its not the same");
+            }
+        }
+
+    }
+}
